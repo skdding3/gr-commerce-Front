7fc12b1a86e054b66455b0ea81284d491a9ee794
skdding3 <skdding3@gmail.com>
2022-07-27 10:19:41 +0900

    [feat] Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Home from "./pages/Home";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  return <Register />;
+  return <Login />;
 }
 
 export default App;
