8125eb2f650465e364987d61a52fe6cbf682fde1
skdding3 <skdding3@gmail.com>
2022-07-30 15:46:44 +0900

    [feat] Responsive Styling Categorie,Navbar,Newsletter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,11 @@ import {
 } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   display: flex;
+  ${mobile({ flexDirection: "column" })}
 `;
 const Left = styled.div`
   flex: 1;
@@ -44,6 +46,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ height: "50px" })}
 `;
 
 const Title = styled.h3`
@@ -68,6 +71,7 @@ const ListItem = styled.li`
 const Right = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
 `;
 
 const ContactItem = styled.div`
