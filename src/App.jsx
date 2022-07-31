e13e8c62b04b73bc3482b2e6805c2ae2bd3c96b0
skdding3 <skdding3@gmail.com>
2022-07-31 16:12:16 +0900

    [feat] Router Path 및 로그인 Route Setting

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,28 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-function App() {
+const App = () => {
+  const user = true; // 회원 있을시 자동으로 홈으로 이동
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/register" element={<Register />} />
+        {/* 로그인 페이지 조건식 */}
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
